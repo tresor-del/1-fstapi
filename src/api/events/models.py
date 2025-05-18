d3fb4e09eae65f5c5c8de2b8b32bd7cb1ea57fa6
tresor-del <tresoradzanto7@gmail.com>
2025-05-18 20:19:05 +0000

    create database models file

---
+++ src/api/events/models.py
@@ -0,0 +1,21 @@
+from typing import List, Optional
+from sqlmodel import SQLModel, Field
+
+
+
+class EventModel(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    page: Optional[str] = ''
+    description: Optional[str] = ''
+
+class EventCreateSchema(SQLModel):
+    page: str
+    description: Optional[str] = Field(default='default description')
+
+class EventUpdateSchema(SQLModel):
+    description: str
+
+
+
+class EventListSchema(SQLModel):
+    results: List[EventModel]
